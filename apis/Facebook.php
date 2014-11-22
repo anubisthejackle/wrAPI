62436ad8b71de65a6eb2367b7c8c46ab6d441722
Travis Weston <tweston@bangordailynews.com>
2014-11-22 10:00:16 -0500

    Add empty methods to Facebook

--- apis/Facebook.php
+++ apis/Facebook.php
@@ -1,4 +1,23 @@
 <?php
 class Facebook implements Api_Interface {
+	
+	public function connect( $api_key ){
 
+	}
+
+	public function get( $path, $options ){
+
+	}
+
+	public function post( $path, $options ){
+
+	}
+
+	public function delete( $path, $options ){
+
+	}
+
+	public function put( $path, $options ){
+
+	}
 }
