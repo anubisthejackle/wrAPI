654f7a4976b429b5fe8af4fd3b5bccd4ac51fe7c
Travis Weston <tweston@bangordailynews.com>
2014-11-22 18:34:07 -0500

    Added sanity checking to wrAPI Exception Autoloader. Checks for Exception existance.

--- wrAPI.php
+++ wrAPI.php
@@ -1,10 +1,17 @@
 <?php
 require_once( dirname( __FILE__ ).'/apis/Api_Interface.php' );
+require_once( dirname( __FILE__ ).'/apis/Abstract_Api.php' );
 
 spl_autoload_register(function( $class ) {
 
 	if( strpos( $class, 'Exception' ) === false )
 		return false;
+
+	if( !file_exists( dirname( __FILE__ ).'/exceptions/'.$class.'.php' ) ){
+
+		throw new Exception( 'Error: Custom exception with name '.$class.' not found.' );
+
+	}
 
 	require_once( dirname( __FILE__ ).'/exceptions/'.$class.'.php' );
 
