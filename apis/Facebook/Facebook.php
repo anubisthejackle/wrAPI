424345dba6027936b532831d41d4c21716e6190d
Travis Weston <tweston@bangordailynews.com>
2014-11-23 00:18:54 -0500

    Facebook API GET Method works.

--- apis/Facebook/Facebook.php
+++ apis/Facebook/Facebook.php
@@ -2,24 +2,24 @@
 class Facebook extends Abstract_Api implements Api_Interface {
 	
 	public function connect( $api_key ){
-		echo 'CONNECTING';
+		// Unused for Facebook
 	}
 
 	public function get( $path, $options ){
 
-		$this->_curl( 'get', $path, $options );
+		return $this->_curl( 'get', $path, $options );
 
 	}
 
 	public function post( $path, $options ){
 
-		$this->_curl( 'post', $path, $options );
+		return $this->_curl( 'post', $path, $options );
 
 	}
 
 	public function delete( $path, $options ){
 
-		$this->_curl( 'delete', $path, $options );
+		return $this->_curl( 'delete', $path, $options );
 
 	}
 
@@ -29,12 +29,35 @@ class Facebook extends Abstract_Api implements Api_Interface {
 
 	private function _curl( $method, $path, $options ) {
 
+		$curl = curl_init();
+		switch( strtolower( $method ) ){
+			case 'get':
+				$params = '?';
+				foreach( $options as $key => $value ){
+					$params .= urlencode( $key ) . '=' . urlencode( $value );
+				}
+				
+				if( $params != '?' ){
+					$path .= $params;
+				}
+				break;
+			case 'post':
+				break;
+			case 'delete':
+				break;
+			default:
+				break;
+		}		
+
+		curl_setopt( $curl, CURLOPT_URL, 'https://graph.facebook.com'.$path );
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
 		
+		return $this->_validate( curl_exec( $curl ) );
 
 	}
 
 	/*** INHERITED ***/
-	private function _validate( $json, $assoc_array = False ) {
+	protected function _validate( $json, $assoc_array = False ) {
 		return parent::_validate( $json, $assoc_array );
 	}
 
--- wrAPI.php
+++ wrAPI.php
@@ -41,5 +41,3 @@ class wrAPI {
 
 }
 
-$fb = wrAPI::create('Facebook');
-$fb->connect( 'TEST' );
