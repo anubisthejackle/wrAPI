ef078615c3cc155a5bd0d4807099a8a72dcbede2
Travis Weston <tweston@bangordailynews.com>
2014-11-24 20:33:33 -0500

    Add blank Twitter extension

---
+++ apis/Twitter/Twitter.php
@@ -0,0 +1,25 @@
+<?php
+
+class Facebook extends Abstract_Api implements Api_Interface {
+
+	public function get( $path, $options ) {
+
+	}
+
+	public function post( $path, $options ) {
+
+	}
+
+	public function delete( $path, $options ) {
+
+	}
+
+	public function put( $path, $options ) {
+
+	}
+
+	public function connect( $api_key ) {
+
+	}
+
+}
