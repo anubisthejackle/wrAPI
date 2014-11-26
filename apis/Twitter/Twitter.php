91b7d0c27994d87e6482c0777264d7675fed472a
Travis Weston <tweston@bangordailynews.com>
2014-11-25 19:33:56 -0500

    Added URL to Twitter

--- apis/Twitter/Twitter.php
+++ apis/Twitter/Twitter.php
@@ -1,25 +1,31 @@
 <?php
+class Twitter extends Abstract_Api implements Api_Interface {
+	
+	public function get( $path, $options ){
 
-class Facebook extends Abstract_Api implements Api_Interface {
-
-	public function get( $path, $options ) {
-
-	}
-
-	public function post( $path, $options ) {
+		return $this->_curl( 'get', $path, $options );
 
 	}
 
-	public function delete( $path, $options ) {
+	public function post( $path, $options ){
+
+		return $this->_curl( 'post', $path, $options );
 
 	}
 
-	public function put( $path, $options ) {
+	public function delete( $path, $options ){
+
+		return $this->_curl( 'delete', $path, $options );
 
 	}
 
-	public function connect( $api_key ) {
-
+	protected function _curl( $method, $path, $options ) {
+		return parent::_curl( $method, 'https://api.twitter.com/1.1'.$path, $options );
 	}
 
+	/*
+	 * Unused
+	 */
+	public function connect( $api_key ){}
+	public function put( $path, $options ){}
 }
