882de435f0aa04549b1abc00c35d69d045aab977
Travis Weston <tweston@bangordailynews.com>
2014-11-25 19:16:23 -0500

    Updated to now include magic methods

--- apis/Facebook/Facebook.php
+++ apis/Facebook/Facebook.php
@@ -1,7 +1,7 @@
 <?php
 class Facebook extends Abstract_Api implements Api_Interface {
 	
-	public function get( $path, $options ){
+/*	public function get( $path, $options ){
 
 		return $this->_curl( 'get', $path, $options );
 
@@ -22,7 +22,11 @@ class Facebook extends Abstract_Api implements Api_Interface {
 	protected function _curl( $method, $path, $options ) {
 		return parent::_curl( $method, 'https://graph.facebook.com'.$path, $options );
 	}
+*/
 
+	public function __call( $name, $arguments ) {
+		return $this->_curl( strtolower( $name ), $arguments[ 0 ], $arguments[ 1 ] );
+	}
 	/*
 	 * Unused
 	 */
