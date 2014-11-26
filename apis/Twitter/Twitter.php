c145b62892e1c57f256adfc4edd6e043f5030d89
Travis Weston <tweston@bangordailynews.com>
2014-11-26 17:49:53 -0500

    Twitter extension

--- apis/Twitter/Twitter.php
+++ apis/Twitter/Twitter.php
@@ -1,31 +1,17 @@
 <?php
 class Twitter extends Abstract_Api implements Api_Interface {
 	
-	public function get( $path, $options ){
+	public function __call( $name, $arguments ) {
 
-		return $this->_curl( 'get', $path, $options );
-
-	}
-
-	public function post( $path, $options ){
-
-		return $this->_curl( 'post', $path, $options );
+		return $this->_curl( 
+					strtolower( $name ), 
+					'https://api.twitter.com/1.1' . $arguments[ 0 ], 
+					$arguments[ 1 ] 
+				);
 
 	}
-
-	public function delete( $path, $options ){
-
-		return $this->_curl( 'delete', $path, $options );
-
-	}
-
-	protected function _curl( $method, $path, $options ) {
-		return parent::_curl( $method, 'https://api.twitter.com/1.1'.$path, $options );
-	}
-
-	/*
-	 * Unused
-	 */
+	
 	public function connect( $api_key ){}
 	public function put( $path, $options ){}
+
 }
