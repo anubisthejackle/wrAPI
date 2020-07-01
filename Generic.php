be2279b96690326c33924092cbd085404c1be29f
Travis Weston <tweston2013@gmail.com>
2020-07-01 14:28:20 -0400

    Create a Generic API candidate

---
+++ Generic.php
@@ -0,0 +1,24 @@
+<?php
+
+class Generic extends Abstract_Api implements Api_Interface
+{
+
+    public function __call( $name, $arguments ) {
+
+		$data = isset($arguments[1]) ? $arguments[1] : null;
+
+		return $this->_curl(
+					strtolower( $name ),
+					$arguments[ 0 ],
+					$data
+				);
+
+	}
+	
+	public function addCustomHeader( $customHeader ){
+
+		$this->_addCustomHeader( $customHeader );
+
+    }
+    
+}
